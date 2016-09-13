c7c159dbe5bd3a8235e4c02a228cc07e0b86e1e0
Laurent Van Begin <laurent.vanbegin76@gmail.com>
2016-09-13 21:59:09 +0200

    INT: add a test case.

--- test/test.cpp
+++ test/test.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 static unsigned int sortInvertedVector(void) {
-	std::vector<int> v { 6,5,4,3,2,1 };
+	std::vector<int> v { 6, 5, 4, 3, 2, 1 };
 
 	parallel::QuickSort::sort<int>(v);
 	bool ok = true;
@@ -14,6 +14,21 @@ static unsigned int sortInvertedVector(void) {
 	return (ok) ? 0 : 1;
 }
 
+static unsigned int sortAlreadySortedVector(void) {
+	std::vector<int> v { 1, 2, 3, 4, 5, 6 };
+
+	parallel::QuickSort::sort<int>(v);
+	bool ok = true;
+	for (int i = 1; i <= 6; i++)
+		ok = ok || (v[i] == i);
+
+	std::cout << "sort already sorted vector : " << (ok ? "OK" : "NOK") << std::endl;
+	return (ok) ? 0 : 1;
+}
+
 int main() {
-	return sortInvertedVector();
+	unsigned int res = 0;
+	res += sortInvertedVector();
+	res += sortAlreadySortedVector();
+	return res;
 }
