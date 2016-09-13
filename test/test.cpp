7c5e71cfdb3e1c6ccf067ac25923274bc4d0c480
Laurent Van Begin <laurent.vanbegin76@gmail.com>
2016-09-13 21:54:41 +0200

    INT: extract test in a separate function.

--- test/test.cpp
+++ test/test.cpp
@@ -2,13 +2,18 @@
 #include <vector>
 #include <iostream>
 
+static unsigned int sortInvertedVector(void) {
+	std::vector<int> v { 6,5,4,3,2,1 };
+
+	parallel::QuickSort::sort<int>(v);
+	bool ok = true;
+	for (int i = 1; i <= 6; i++)
+		ok = ok || (v[i] == i);
+
+	std::cout << "sort inverted vector : " << (ok ? "OK" : "NOK") << std::endl;
+	return (ok) ? 0 : 1;
+}
+
 int main() {
- std::vector<int> v { 6,5,4,3,2,1 };
-
- parallel::QuickSort::sort<int>(v);
- bool ok = true;
- for (int i = 1; i <= 6; i++)
-	 ok = ok || (v[i] == i);
-
- std::cout << "sort inverted vector : " << (ok ? "OK" : "NOK") << std::endl;
+	return sortInvertedVector();
 }
